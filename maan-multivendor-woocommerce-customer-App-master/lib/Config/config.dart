9148ddae00892e857f820ee3ed4a154c446c0da9
melsayedahmed <mohab.moh7000@gmail.com>
2024-12-25 15:01:20 +0200

    first commit

---
+++ maan-multivendor-woocommerce-customer-App-master/lib/Config/config.dart
@@ -0,0 +1,18 @@
+class Config {
+  static String websiteURL = 'https://ecommerceapp.acnoo.com/multivendor-customer-app/';
+  static String key = 'ck_53d3d1a126f85ccfd0a95a7ce0c19803f0ed8523';
+  static String secret = 'cs_010c14e8907821b586e8961eb5548bbe9210b46d';
+  static String url = 'https://ecommerceapp.acnoo.com/multivendor-customer-app/wp-json/wc/v3/';
+  static String orderConfirmUrl = 'https://ecommerceapp.acnoo.com/multivendor-customer-app/checkout/order-received/';
+  static String customerURL = 'customers';
+  static String tokenURL = 'https://ecommerceapp.acnoo.com/multivendor-customer-app/wp-json/jwt-auth/v1/token';
+  static String categoryURL = 'products/categories';
+  static String productsURL = 'products';
+  static String singleProductsURL = 'products/';
+  static String customerDetails = 'customers/';
+  static String createOrderUrl = 'orders';
+  static String anOrderUrl = 'orders/';
+  static String coupons = 'coupons';
+  static String media = 'https://ecommerceapp.acnoo.com/multivendor-customer-app/wp-json/wp/v2/media?';
+  static String createReviewUrl = 'products/reviews';
+}
